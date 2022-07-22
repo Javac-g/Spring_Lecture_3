8fa5386602126fbbc887649925a40a21c43b51c2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-23 00:20:14 +0300

    print

---
+++ MainVersion/Service/Service.java
@@ -0,0 +1,12 @@
+package com.company.MainVersion.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+    private List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(String msg){
+        return msg;
+    }
+}
