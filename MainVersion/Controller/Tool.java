bee977af67f5e415b5959978a755aca51944d945
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-22 23:41:38 +0300

    Tool

---
+++ MainVersion/Controller/Tool.java
@@ -0,0 +1,21 @@
+package com.company.MainVersion.Controller;
+
+public class Tool {
+    private String first,second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+}
