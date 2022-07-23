4c3878532194e4feeaf198b2d58a96e8f6ca0f2b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-23 17:25:41 +0300

    Service

---
+++ MainVersion/Controller/Controller.java
@@ -0,0 +1,12 @@
+package com.company.MainVersion.Controller;
+
+import com.company.MainVersion.Service.Service;
+
+public class Controller {
+    private Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+}
--- MainVersion/Service/Service.java
+++ MainVersion/Service/Service.java
@@ -1,5 +1,11 @@
 package com.company.MainVersion.Service;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +14,24 @@ public class Service {
 
     public String print(String msg){
         return msg;
+    }
+    public void serviceLog(String type,ResponseDTO user){
+
+        byte[] data = ("\nType: " + type + "\nName: " + user.getName() + "\nId: " + user.getId()).getBytes();
+        String add = "\nTool - First: " + user.getTool().getFirst() + "\nTool - Second: " + user.getTool().getSecond() + "\nCar: " + user.getCarenum();
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream("log.dat",true);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+            byteArrayOutputStream.write(data);
+            byteArrayOutputStream.writeTo(fileOutputStream);
+            dataOutputStream.writeUTF(add);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
