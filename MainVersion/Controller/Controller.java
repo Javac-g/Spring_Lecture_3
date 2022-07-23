2be5ec010f8e44da69924fd09fcd9fea74b330cc
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-23 18:15:41 +0300

    delete

--- MainVersion/Controller/Controller.java
+++ MainVersion/Controller/Controller.java
@@ -5,10 +5,7 @@ import com.company.MainVersion.Service.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/Main")
 public class Controller {
@@ -24,6 +21,39 @@ public class Controller {
         log.info("\nhttp://192.168.0.102:8080/Main/a");
         log.info("Created: " + user);
         return service.create(user);
+    }
+    @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO findData(@RequestParam Integer id){
+        log.info("\nhttp://192.168.0.102:8080/Main/f?id=" + id);
+        return service.read(id);
+    }
+    @PutMapping(value = "/u",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO updateData(@RequestParam Integer id, @RequestBody RequestDTO json){
+        log.info("\nhttp://192.168.0.102:8080/Main/u?id=" + id);
+        log.info("\nJSON: " + json);
+        return service.update(id, json);
+    }
+
+    @DeleteMapping(value = "/d",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String deleteData(@RequestParam Integer id){
+
+        if (id > 2){
+
+            log.info("\nhttp://192.168.0.102:8080/Main/d -- DELETE A");
+
+            return service.print("Was deleted person N: " + service.Delete_A(id));
+
+        } else if (id <=2) {
+
+            log.info("\nhttp://192.168.0.102:8080/Main/d -- DELETE B");
+
+            return service.print("Was deleted person N: ") + service.Delete_B(id);
+        }
+
+        return "Nobody deleted!";
     }
 
 }
